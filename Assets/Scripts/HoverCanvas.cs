fd535adc93eb040f0d753b9d7eb07d6518c14b07
Kok Han Yi <31101286+hannukk@users.noreply.github.com>
2018-01-28 00:51:30 +0800

    text

--- Assets/Scripts/HoverCanvas.cs
+++ Assets/Scripts/HoverCanvas.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Planet : MonoBehaviour {
+public class HoverCanvas : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+		
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate(Vector3.up, 0.1f);
+		
 	}
 }
