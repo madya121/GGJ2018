26f049d753821e65d522370c9b9be3fc411d58e2
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2018-01-28 03:41:32 +0800

    StatBar tweaks + Planet prefab

--- Assets/Scripts/StatBar.cs
+++ Assets/Scripts/StatBar.cs
@@ -8,17 +8,32 @@ public class StatBar : MonoBehaviour {
     public GameObject navy, green, red;
     private Planet planet;
 
-	// Use this for initialization
-	void Start () {
+    double radius;
+
+    // Use this for initialization
+    void Start () {
         planet = transform.parent.gameObject.GetComponent<Planet>();
+        radius = planet.GetComponent<Renderer>().bounds.size.x;
+        float multiplier = Math.Max(0.4f, (float)radius / 8);
+        navy.GetComponent<LineRenderer>().widthMultiplier = multiplier;
+        green.GetComponent<LineRenderer>().widthMultiplier = multiplier;
+        red.GetComponent<LineRenderer>().widthMultiplier = multiplier;
         //red = new GameObject();
     }
-	
+
 	// Update is called once per frame
 	void Update () {
         float popPercent = (float)(planet.planetData.Population / planet.planetData.MaxPopulation);
         float infPercent = (float)(planet.planetData.InfectedPopulation / planet.planetData.MaxPopulation);
         red.transform.localScale = new Vector3(0, 0, infPercent);
         green.transform.localScale = new Vector3(0, 0, popPercent);
+
+        //double width = red.GetComponent<Renderer>().bounds.size.x;
+        if (Input.GetKeyDown("z"))
+        {
+            Debug.Log("Planet " + this.name + " radius: " + radius);
+        }
+
+        
 	}
 }
