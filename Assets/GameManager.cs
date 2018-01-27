6f1c2d108bcfcc22ae4b7f4e3cae4f199f9ddd69
Vincentius Madya Putra <vincentius.madya@grabtaxi.com>
2018-01-27 22:20:42 +0800

    Add timer

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,12 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
   public GameObject gameOverCanvas;
   public GameObject diseaseCanvas;
 
+  public Animator timeTextAnimator;
+  public Text timeText;
+  public Text secondTimeText;
+  public float timeInSecond;
+  public Color textColorBegin, textColorEnd;
+  private float secondPassed = 1.0f;
+
+  [HideInInspector]
   public bool isGameOver = false;
 
 	// Use this for initialization
@@ -16,10 +25,29 @@ public class GameManager : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-    if (!isGameOver && Input.GetKeyDown(KeyCode.Escape)) {
-      isGameOver = true;
-      diseaseCanvas.SetActive(false);
-      gameOverCanvas.SetActive(true);
+    timeInSecond -= Time.deltaTime;
+    secondPassed -= Time.deltaTime;
+
+    if (!isGameOver && timeInSecond < 0) {
+        isGameOver = true;
+        diseaseCanvas.SetActive(false);
+        gameOverCanvas.SetActive(true);
+    }
+
+    if (isGameOver)
+      return;
+
+    int years = (int)((timeInSecond / 60.0f) * 1000.0f);
+    if (years < 0)
+      years = 0;
+    timeText.text = years + " YEARS";
+    secondTimeText.text = years + " YEARS";
+    Color lerpedColor = Color.Lerp(textColorBegin, textColorEnd, 1.0f - (timeInSecond / 60.0f));
+    timeText.color = lerpedColor;
+    secondTimeText.color = lerpedColor;
+    if (secondPassed < 0.0f) {
+      secondPassed = 1.0f;
+      timeTextAnimator.SetTrigger("Boom");
     }
 	}
 
