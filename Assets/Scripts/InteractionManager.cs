2e2362194d5e2cef5b3cf49d53c70c68c8450b14
Vincentius Madya Putra <vincentius.madya@grabtaxi.com>
2018-01-27 10:21:27 +0800

    Simple Interaction

---
+++ Assets/Scripts/InteractionManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionManager : MonoBehaviour {
+
+  public Sprite sprite;
+
+  [HideInInspector]
+  public int activeDiseaseIndex = -1;
+  public Sprite diseaseImage;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+    if (activeDiseaseIndex > -1 && Input.GetMouseButtonUp(0)) {
+      GameObject obj = GameObject.Find("Helper").GetComponent<Helper>().GetObjectFromMousePosition();
+      if (obj) {
+      if (activeDiseaseIndex == 0)
+          obj.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.green);
+        else if (activeDiseaseIndex == 1)
+          obj.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+      }
+
+      activeDiseaseIndex = -1;
+      GameObject.Find("Helper").GetComponent<Helper>().DettachSpriteFromMousePosition();
+    }
+    if (activeDiseaseIndex > -1)
+      GameObject.Find("Helper").GetComponent<Helper>().AttachSpriteToMousePosition(diseaseImage);
+	}
+}
