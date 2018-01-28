c0dc1c20d651c949ce5c33f03a8538ba7cf5830c
Vincentius Madya Putra <vincentius.madya@grabtaxi.com>
2018-01-28 12:13:37 +0800

    Add main menu and cartoon texture

---
+++ Assets/MainMenuManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour {
+
+  public Animator animator;
+  public GameObject overlay;
+  private bool changeScene = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+  public void GoToPlayScene() {
+    Debug.Log("asd");
+    overlay.SetActive(true);
+    if (changeScene)
+      return;
+    changeScene = true;
+
+    animator.SetTrigger("Change Scene");
+  }
+}
